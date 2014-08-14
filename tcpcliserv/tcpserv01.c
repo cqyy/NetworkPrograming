ed5ff669bfcd465ca65be766d91249360deee59f
root <root@localhost.localdomain>
2014-08-15 07:38:18 +0800

    echo server

---
+++ tcpcliserv/tcpserv01.c
@@ -0,0 +1,28 @@
+#include<../header>
+
+int main(int argc,char **argv)
+{
+	sockaddr_t addr,cliaddr;
+	int serfd,clifd;
+	int clilen;
+	int chipid;
+
+	bzero(&addr,sizeof(addr));
+	serfd = sock(AF_INET,SOCK_STREAM,0);
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	addr.sin_port = 9002;
+	Bind(serfd,(struct sockaddr*)&addr,sizeof(addr));
+	Listen(serfd,10);
+
+	for(;;){
+		clilen = sizeof(cliaddr);
+		clifd = accept(serfd,(struct sockaddr*)&cliaddr,&clilen);
+		if( (chipid = fork()) == 0){
+			close(serfd);
+			str_echo(clifd);
+			close(clifd);
+		}
+		close(clifd);
+	}
+}

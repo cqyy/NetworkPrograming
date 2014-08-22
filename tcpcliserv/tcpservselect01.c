cced685ee9b3de58da964ef70d2680b3eeb0d261
root <root@localhost.localdomain>
2014-08-22 08:04:58 +0800

    select

--- tcpcliserv/tcpservselect01.c
+++ tcpcliserv/tcpservselect01.c
@@ -7,7 +7,8 @@ int main(int argc,char **argv)
 	char buf[100];
 	socklen_t clilen;
 	struct sockaddr_in cliaddr,servaddr;
-
+	
+	maxi = 0;
 	listenfd = socket(AF_INET,SOCK_STREAM,0);
 	maxfd = listenfd;
 
@@ -15,4 +16,35 @@ int main(int argc,char **argv)
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(9877);
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	for(i = 0; i < 100; i++){
+		client[i] = -1;
+	}
+	Bind(listenfd,&servaddr,sizeof(srevaddr));
+	Listen(listenfd,100);
+	FD_ZERO(&allset);
+	FD_SET(listenfd,&allset);
+
+	for(;;){
+		rset = allset;
+		nready = select(maxfd+1,&rset,NULL,NULL,NULL);
+		
+		if(FD_ISSET(listenfd,&rset)){
+			clilen = sizeof(cliaddr);
+			connfd = accept(listenfd,(struct sockaddr*)&cliaddr,&clilen);
+			for(i = 0; i < 100; i++){
+				if(client[i] < 0){
+					client[i] = connfd;
+					if(connfd > maxfd)
+						maxfd = connfd;
+					if(i > maxi)
+						maxi = i;
+					}
+			}
+			if( i == 100){
+				fputs("too much connection\n",stderr);
+				exit(1);
+			}
+		}
+	}
 }

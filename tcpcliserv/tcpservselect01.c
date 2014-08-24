b57f53268a410ccc893b2c166c12ec671aaa1b62
root <root@localhost.localdomain>
2014-08-25 06:23:15 +0800

    basic select server

--- tcpcliserv/tcpservselect01.c
+++ tcpcliserv/tcpservselect01.c
@@ -1,3 +1,5 @@
+#include "../header.h"
+
 int main(int argc,char **argv)
 {
 	int client[100],nready;
@@ -20,15 +22,15 @@ int main(int argc,char **argv)
 	for(i = 0; i < 100; i++){
 		client[i] = -1;
 	}
-	Bind(listenfd,&servaddr,sizeof(srevaddr));
+	Bind(listenfd,(struct sockaddr*)&servaddr,sizeof(servaddr));
 	Listen(listenfd,100);
 	FD_ZERO(&allset);
 	FD_SET(listenfd,&allset);
-
+	printf("server start at post %d\n",9877);
 	for(;;){
 		rset = allset;
 		nready = select(maxfd+1,&rset,NULL,NULL,NULL);
-		
+		/*receive new connection*/	
 		if(FD_ISSET(listenfd,&rset)){
 			clilen = sizeof(cliaddr);
 			connfd = accept(listenfd,(struct sockaddr*)&cliaddr,&clilen);
@@ -39,11 +41,30 @@ int main(int argc,char **argv)
 						maxfd = connfd;
 					if(i > maxi)
 						maxi = i;
+					break;
 					}
 			}
 			if( i == 100){
 				fputs("too much connection\n",stderr);
 				exit(1);
+			}
+			if ( --nready == 0){
+				continue;
+			}
+		}
+		/*read and write back*/
+		for( i = 0; i < maxi; i++){
+			if( (sockfd = client[i]) < 0)
+				continue;
+			if( FD_ISSET(sockfd,&rset)){
+				if ( (n = read(sockfd,buf,sizeof(buf))) == 0){
+					close(sockfd);
+					client[i] = -1;
+					FD_CLR(sockfd,&allset);
+				}else
+					writen(sockfd,buf,n);
+				if( --nready == 0)
+					continue;
 			}
 		}
 	}
